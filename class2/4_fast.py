0f09cbe9f21a2035c989a870049b0b48bb525ee8
Ryan Vredenburg <rvredenburg@coeur.com>
2020-02-03 19:33:58 -0800

    Completed class 2 exercise 4

---
+++ class2/4_fast.py
@@ -0,0 +1,37 @@
+"""
+4. Use Netmiko and the send_config_set() method to configure the following on the Cisco3 router.
+
+ip name-server 1.1.1.1
+ip name-server 1.0.0.1
+ip domain-lookup
+
+Experiment with fast_cli=True to see how long the script takes to execute (with and without this option enabled).
+
+Verify DNS lookups on the router are now working by executing 'ping google.com'. Verify from this that you receive a ping response back.
+
+"""
+
+from netmiko import ConnectHandler
+from getpass import getpass
+
+password = getpass()
+device  = {
+    "host": 'cisco3.lasthop.io', 
+    "username": 'pyclass',
+    "password": password, 
+    "device_type": 'cisco_ios',
+    "fast_cli": True 
+}
+
+cfg = [
+    'ip name-server 1.1.1.1',
+    'ip name-server 1.0.0.1',
+    'ip domain-lookup'
+]
+
+net_connect = ConnectHandler(**device)
+
+print(net_connect.send_config_set(cfg))
+
+print(net_connect.send_command('ping google.com'))
+
